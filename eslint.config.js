d20a77f5b0ebc2a6e4f540c08c41ab29352a1ede
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2025-11-18 12:07:26 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,21 @@ export default defineConfig([
       },
     },
   },
+    {
+    rules: {
+      'brace-style': 'error',
+      'comma-dangle': ['error', 'never'],
+      'indent': ['error', 2],
+      'no-eval': 'error',
+      'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 0 }],
+      'no-trailing-spaces': 'error',
+      'no-unused-vars': 'error',
+      'no-var': 'error',
+      'prefer-const': ['error'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always']
+    }
+  },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
