098e052a400abec62d9461fb09d12bd5858c0798
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2025-11-24 10:30:16 +0100

    login made as layout none

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ function generateRoutes(items) {
         path: item.path,
         name: item.label.toLowerCase().replace(/\s+/g, ""),
         component: item.view,
+        meta: item.meta || {} 
       })
     }
 
