c226f173b9e89791c17b7a8205cfcf69f8257cf8
khni48704 <hjerno.kasper@gmail.com>
2025-11-25 10:25:07 +0100

    Sales-page, styling and setup

---
+++ src/components/filter/SearchBar.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+        <p>hej fra Serchbar comp</p>
+
+</template>
--- src/views/SalesView.vue
+++ src/views/SalesView.vue
@@ -1,44 +1,72 @@
 <script setup>
+import { ref } from "vue";
 import BreadcrumbsComp from '@/components/navigation/BreadcrumbsComp.vue';
+import CarsInNumbers from "@/components/filter/CarsInNumbers.vue";
+import SearchBar from "@/components/filter/SearchBar.vue";
 
-const salesCustomers = [
- "Auto-House Køge Hvidovre",
- "Autotorvet",
- "Billi Billi",
- "RST Biler",
- "SIXT Bilsag",
- "Thomens Auto",
-]
+const salesCustomers = ref([
+  "Auto-House Køge Hvidovre",
+  "Autotorvet",
+  "Billi Billi",
+  "RST Biler",
+  "SIXT Bilsag",
+  "Thomens Auto",
+]);
 
+const selectedCustomers = ref([]);
 
+// Flyt fra venstre -> højre
+function moveToSelected(customer) {
+  salesCustomers.value = salesCustomers.value.filter(c => c !== customer);
+  selectedCustomers.value.push(customer);
+}
+
+function SelectAllSelected(customer){
+  selectedCustomers.value = salesCustomers.value.filter (c=> c !== customer);
+}
+
+function selectAllCustomers() {
+  selectedCustomers.value.push(...salesCustomers.value);
+  salesCustomers.value = [];
+}
+
+function removeAllCustomers() {
+  salesCustomers.value.push(...selectedCustomers.value);
+  selectedCustomers.value = [];
+}
+
+// Flyt fra højre -> venstre
+function moveToAvailable(customer) {
+  selectedCustomers.value = selectedCustomers.value.filter(c => c !== customer);
+  salesCustomers.value.push(customer);
+}
 </script>
+
 <template>
-  
+  <div class="SalesView">
+    <div class="SalesView__topbar"> <h1>Salg</h1>
+      <button class="SalesView__button-next">Vis valgte</button>
+  </div>
 
-
-<div class="SalesView">
-
-    <div class="SalesView__topbar">
-      <h1>Salg</h1>
-    </div>
-
-    <p class="regular settings-breadcrumbs">  <!--settings breadcrum??? -->
-      <BreadcrumbsComp />
-    </p>
-
-    <p>filter funktioner her!!! (Søg og antal biler)</p>
-
+  <p class="regular settings-breadcrumbs"> <!--settings breadcrum??? --> <BreadcrumbsComp /> </p>
+<div class="SalesView__filter-section">
+<CarsInNumbers/>
+<SearchBar/>
+</div>
     <!-- Ikke valgte kunder -->
+  <div class="SalesView__wrapper"> 
     <div class="SalesView__customer-list">
       <div class="SalesView__controls">
         <p class="text-medium">Virksomheder</p>
-        <button class="SalesView__button">Vælg alle</button>
+        <button class="SalesView__button" @click="selectAllCustomers">Vælg alle</button>
       </div>
+
       <ul>
         <li
           v-for="item in salesCustomers"
           :key="item"
           class="SalesView__customer-item h3"
+          @click="moveToSelected(item)"
         >
           {{ item }}
         </li>
@@ -47,9 +75,21 @@ const salesCustomers = [
 
     <!-- Valgte kunder -->
     <div class="SalesView__customer-list SalesView__customer-list--selected">
+      <div class="SalesView__controls">
+      <p class="text-medium">Valgte kunder</p>
+      <button class="SalesView__button" @click="removeAllCustomers">Fjern alle</button>
     </div>
-
+      <ul>
+        <li
+          v-for="item in selectedCustomers"
+          :key="item"
+          class="SalesView__customer-item h3"
+          @click="moveToAvailable(item)"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </div>
-
-
 </template>
