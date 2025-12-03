bf711a3c5183c1a7f43a1d3eaeb99105e7fad621
khni48704 <hjerno.kasper@gmail.com>
2025-12-03 09:34:51 +0100

    chart mock data

--- src/components/CarBoostGraph.vue
+++ src/components/CarBoostGraph.vue
@@ -1,25 +1,26 @@
 <script setup>
+import { onMounted } from 'vue'
 import ApexCharts from 'apexcharts'
 
-var options = {
-  chart: {
-    type: 'line'
-  },
-  series: [{
-    name: 'sales',
-    data: [30,40,35,50,49,60,70,91,125]
-  }],
-  xaxis: {
-    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+onMounted(() => {
+  const options = {
+    chart: {
+      type: 'line'
+    },
+    series: [{
+      name: 'sales',
+      data: [30,40,35,50,49,60,70,91,125]
+    }],
+    xaxis: {
+      categories: [1991,1992,1993,1994,1995,1996,1997,1998,1999]
+    }
   }
-}
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-
-chart.render();
+  const chart = new ApexCharts(document.querySelector("#chart"), options)
+  chart.render()
+})
 </script>
 
 <template>
-
-    
+  <div id="chart"></div>
 </template>
