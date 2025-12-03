5545542f70cd1c36832fd1f3751c79702d157649
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2025-12-03 09:25:30 +0100

    btn made to switch to graph

--- src/views/CarBoostView.vue
+++ src/views/CarBoostView.vue
@@ -1,20 +1,31 @@
 <script setup>
+import { ref } from 'vue';
 import BreadcrumbsComp from '@/components/navigation/BreadcrumbsComp.vue';
 import CarBoostTable from '@/components/CarBoostTable.vue';
 import SearchBar from '@/components/filter/SearchBar.vue';
 import DisplayComp from '@/components/filter/DisplayComp.vue';
 import CalendarComp from '@/components/filter/CalendarComp.vue';
+import CarBoostGraph from '@/components/CarBoostGraph.vue';
+
+const showGraph = ref(false);
+
 
 </script>
 <template>  
-  <div class="car-boost-view">
-    <h1 class="carboost-view-title">CarBoost</h1>
-    <BreadcrumbsComp/>
+  <div class="carboost-view">
+    <div class="carboost-view__topbar">
+      <div class="carboost-view__topbar-left">
+        <h1 class="carboost-view__topbar-title">CarBoost</h1>
+        <BreadcrumbsComp/> 
+      </div>   
+      <button class="carboost-view__topbar-btn" @click="showGraph = !showGraph">Vis valgte</button>
+    </div>
     <div class="carboost-view__filter">
       <SearchBar />
       <DisplayComp />
       <CalendarComp />
     </div>
-    <CarBoostTable />
+    <CarBoostTable v-if="!showGraph" />
+    <CarBoostGraph v-else />
   </div>
 </template>
