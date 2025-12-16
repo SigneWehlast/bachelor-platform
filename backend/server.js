8cce7d1d79b533ee3e1bc161530e24b5ef253870
khni48704 <hjerno.kasper@gmail.com>
2025-12-16 10:55:33 +0100

    cron job remove and added to separately terminal

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, ".env") });
 
 import app from "./app.js";
-import "./cron/dailySync.js";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
