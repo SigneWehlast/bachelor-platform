0ee974b531a784547426864432d8600c7b2e0930
khni48704 <hjerno.kasper@gmail.com>
2025-12-17 10:58:32 +0100

    bell add as component

---
+++ src/components/navigation/NotifikacationBell.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Icon from "@/components/Icon.vue"
+
+</script>
+
+<template>
+        <div class="nontificationBell">
+        <Icon name="BellOutline" class="nontificationBell__icon" />
+
+        </div>
+</template>
