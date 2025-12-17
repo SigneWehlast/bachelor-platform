de7f320a035fee0c6750ee8f25290a499be75bfa
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2025-12-17 12:25:15 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-
+/*
 router.beforeEach((to, from, next) => {
   const isLoggedIn = !!localStorage.getItem("token")
 
@@ -40,7 +40,7 @@ router.beforeEach((to, from, next) => {
   }
 
   next()
-})
+})*/
 
 router.afterEach((to) => {
   UserTracking(to.path)
