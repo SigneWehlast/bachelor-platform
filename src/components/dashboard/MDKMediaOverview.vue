1f36dc6ac5a21af014c7b060dcb572b2a6f6afbd
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2025-12-20 15:38:47 +0100

    warnings on dashboard is working automatically

--- src/components/dashboard/MDKMediaOverview.vue
+++ src/components/dashboard/MDKMediaOverview.vue
@@ -1,31 +1,46 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { getCustomersInGroups } from '@/services/groupOverviewService';
+import { useCarboostWarnings } from '@/utils/CarboostWarnings.js';
+import { getCustomersInCarboost } from '@/services/carboostService';
 
 const overviews = ref([]);
 
-const notifications = ref({name: "Advarsler", data: "2" });
+const { setCustomers, warningCustomers } = useCarboostWarnings();
+
+const combinedOverview = computed(() => {
+  const warningBox = {
+    name: 'Advarsler',
+    data: warningCustomers.value.length
+  };
+
+  return [warningBox, ...overviews.value];
+});
 
 onMounted(async () => {
   const data = await getCustomersInGroups();
-    overviews.value = data.map(item => ({
+  overviews.value = data.map(item => ({
     name: item.groupName,
-    data: item.customerCount 
+    data: item.customerCount
   }));
+
+  const carboostData = await getCustomersInCarboost(1, 99999);
+  setCustomers(carboostData.customers || []);
 });
 </script>
+
 <template>
-    <div class="mdkmedia-overview">
-        <h1 class="mdkmedia-overview__title">MDK-Media Oversigt</h1>
-        <div class="mdkmedia-overview__content">
-           <div class="mdkmedia-overview__content__box">
-                <div class="mdkmedia-overview__content__box-name h3">{{ notifications.name }}</div>
-                <div class="mdkmedia-overview__content__box-data h1">{{ notifications.data }}</div>
-           </div>
-            <div v-for="(overview, index) in overviews" :key="index" class="mdkmedia-overview__content__box">
-                <div class="mdkmedia-overview__content__box-name h3">{{ overview.name }}</div>
-                <div class="mdkmedia-overview__content__box-data h1">{{ overview.data }}</div>
-            </div>
-        </div>
+  <div class="mdkmedia-overview">
+    <h1 class="mdkmedia-overview__title">MDK-Media Oversigt</h1>
+    <div class="mdkmedia-overview__content">
+      <div
+        v-for="(overview, index) in combinedOverview"
+        :key="index"
+        class="mdkmedia-overview__content__box"
+      >
+        <div class="mdkmedia-overview__content__box-name h3">{{ overview.name }}</div>
+        <div class="mdkmedia-overview__content__box-data h1">{{ overview.data }}</div>
+      </div>
     </div>
+  </div>
 </template>
