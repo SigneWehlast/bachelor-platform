531d148b4374ba61d72d6259f2b912d64400a4e4
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2025-12-27 20:37:40 +0100

    Update BreadcrumbsComp.vue

--- src/components/navigation/BreadcrumbsComp.vue
+++ src/components/navigation/BreadcrumbsComp.vue
@@ -3,22 +3,29 @@ import { trackingStore } from '@/utils/trackingStore';
 </script>
 
 <template>
-  <div class='breadcrumbs text-light'>
+<div class='breadcrumbs text-light'>
+  <template v-for='(crumb, index) in trackingStore.breadcrumbs' :key='index'>
     <RouterLink
-      v-for='(crumb, index) in trackingStore.breadcrumbs'
-      :key='index'
-      :to='crumb.path'
+      v-if="crumb.path"
+      :to="crumb.path"
       class='breadcrumbs__item'
       :class="{ 'breadcrumbs__item--active': index === trackingStore.breadcrumbs.length - 1 }"
     >
       {{ crumb.label }}
-
-      <span
-        v-if='index < trackingStore.breadcrumbs.length - 1'
-        class='breadcrumbs__separator'
-      >
-        /
-      </span>
     </RouterLink>
-  </div>
+    <span
+      v-else
+      class='breadcrumbs__item'
+    >
+      {{ crumb.label }}
+    </span>
+
+    <span
+      v-if='index < trackingStore.breadcrumbs.length - 1'
+      class='breadcrumbs__separator'
+    >
+      /
+    </span>
+  </template>
+</div>
 </template>
